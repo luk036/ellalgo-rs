677a29160e928e078f280c9fa3e399fc07defc53
Wai-Shing Luk <luk036@gmail.com>
2026-05-30 16:16:53 +0800

    update the bench

--- benches/ellipsoid.rs
+++ benches/ellipsoid.rs
@@ -1,33 +1,24 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use ellalgo_rs::arr::Arr;
-use ellalgo_rs::cutting_plane::{cutting_plane_optim, Options, OracleOptim};
+use ellalgo_rs::cutting_plane::{cutting_plane_optim, Options, OracleOptim, SingleCut};
 use ellalgo_rs::ell::Ell;
 
 #[derive(Debug, Default)]
 pub struct BenchOracle;
 
 impl OracleOptim<Arr> for BenchOracle {
-    type CutChoice = f64;
+    type CutChoice = SingleCut;
 
-    fn assess_optim(&mut self, xc: &Arr, gamma: &mut f64) -> ((Arr, f64), bool) {
-        let x_val = xc[0];
-        let y_val = xc[1];
-        let f0 = x_val + y_val;
-        let f1 = f0 - 3.0;
-        if f1 > 0.0 {
-            return ((Arr::from(vec![1.0, 1.0]), f1), false);
+    fn assess_optim(&mut self, xc: &Arr, gamma: &mut f64) -> ((Arr, SingleCut), bool) {
+        let gradient = 2.0 * xc;
+        let f = xc.dot(xc);
+        if f < *gamma {
+            *gamma = f;
+            ((gradient, SingleCut(f)), true)
+        } else {
+            ((gradient, SingleCut(f)), false)
         }
-        let f2 = -x_val + y_val + 1.0;
-        if f2 > 0.0 {
-            return ((Arr::from(vec![-1.0, 1.0]), f2), false);
-        }
-        let f3 = *gamma - f0;
-        if f3 > 0.0 {
-            return ((Arr::from(vec![-1.0, -1.0]), f3), false);
-        }
-        *gamma = f0;
-        ((Arr::from(vec![-1.0, -1.0]), 0.0), true)
     }
 }
 
